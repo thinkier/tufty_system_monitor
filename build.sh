b105a5ac306657d8f9bf5d9db55fc91ad6e45298
thinkier <26728734+thinkier@users.noreply.github.com>
2022-08-11 00:21:41 +1000

    I made progress. for once!

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ OUT_DIR="../pico-project/artefacts/"
 cmake ../pico-project || exit 1
 make || exit 1
 
-mkdir $OUT_DIR
+mkdir $OUT_DIR >/dev/null 2>&1
 FORMATS=("bin" "dis" "elf" "elf.map" "hex" "uf2")
 for FORMAT in "${FORMATS[@]}"; do
   cp *.$FORMAT $OUT_DIR
--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "pico/stdlib.h"
-#include <stdio.h>
+#include <cstdio>
 #include <cstring>
 #include <string>
 #include <algorithm>
+#include <iostream>
 #include "pico/time.h"
 #include "pico/platform.h"
+#include "pico/multicore.h"
+#include "pico/mutex.h"
+#include "json.hpp"
 
 #include "common/pimoroni_common.hpp"
 #include "drivers/st7789/st7789.hpp"
@@ -12,7 +16,9 @@
 #include "tufty2040.hpp"
 #include "button.hpp"
 
+using json = nlohmann::json;
 using namespace pimoroni;
+using namespace std;
 
 Tufty2040 tufty;
 
@@ -38,125 +44,37 @@ Button button_c(Tufty2040::C);
 Button button_up(Tufty2040::UP);
 Button button_down(Tufty2040::DOWN);
 
-uint32_t time() {
-    absolute_time_t t = get_absolute_time();
-    return to_ms_since_boot(t);
-}
+auto_init_mutex(serial_data_mtx);
+shared_ptr<json> serial_data;
 
-// HSV Conversion expects float inputs in the range of 0.00-1.00 for each channel
-// Outputs are rgb in the range 0-255 for each channel
-void from_hsv(float h, float s, float v, uint8_t &r, uint8_t &g, uint8_t &b) {
-    float i = floor(h * 6.0f);
-    float f = h * 6.0f - i;
-    v *= 255.0f;
-    uint8_t p = v * (1.0f - s);
-    uint8_t q = v * (1.0f - f * s);
-    uint8_t t = v * (1.0f - (1.0f - f) * s);
+void comms() {
+    string data;
+    while (getline(cin, data)) {
+        json j = json::parse(data);
 
-    switch (int(i) % 6) {
-        case 0:
-            r = v;
-            g = t;
-            b = p;
-            break;
-        case 1:
-            r = q;
-            g = v;
-            b = p;
-            break;
-        case 2:
-            r = p;
-            g = v;
-            b = t;
-            break;
-        case 3:
-            r = p;
-            g = q;
-            b = v;
-            break;
-        case 4:
-            r = t;
-            g = p;
-            b = v;
-            break;
-        case 5:
-            r = v;
-            g = p;
-            b = q;
-            break;
+        mutex_enter_blocking(&serial_data_mtx);
+        serial_data = make_shared<json>(j);
+        mutex_exit(&serial_data_mtx);
     }
 }
 
 int main() {
-    st7789.set_backlight(255);
+    stdio_init_all();
+    multicore_launch_core1(comms);
 
-    Pen WHITE = graphics.create_pen(255, 255, 255);
-    Pen BG = graphics.create_pen(120, 40, 60);
+    st7789.set_backlight(128);
 
-    struct pt {
-        float x;
-        float y;
-        uint8_t r;
-        float dx;
-        float dy;
-        uint16_t pen;
-    };
-
-    std::vector<pt> shapes;
-    for (int i = 0; i < 100; i++) {
-        pt shape;
-        shape.x = rand() % graphics.bounds.w;
-        shape.y = rand() % graphics.bounds.h;
-        shape.r = (rand() % 10) + 3;
-        shape.dx = float(rand() % 255) / 64.0f;
-        shape.dy = float(rand() % 255) / 64.0f;
-        shape.pen = graphics.create_pen(rand() % 255, rand() % 255, rand() % 255);
-        shapes.push_back(shape);
-    }
-
-    Point text_location(0, 0);
-    uint8_t i = 0;
+    Pen BG = graphics.create_pen(0, 0, 0);
 
     while (true) {
+        mutex_enter_blocking(&serial_data_mtx);
+        cout << serial_data << endl;
+        mutex_exit(&serial_data_mtx);
 
         graphics.set_pen(BG);
         graphics.clear();
 
-        for (auto &shape: shapes) {
-            shape.x += shape.dx;
-            shape.y += shape.dy;
-            if ((shape.x - shape.r) < 0) {
-                shape.dx *= -1;
-                shape.x = shape.r;
-            }
-            if ((shape.x + shape.r) >= graphics.bounds.w) {
-                shape.dx *= -1;
-                shape.x = graphics.bounds.w - shape.r;
-            }
-            if ((shape.y - shape.r) < 0) {
-                shape.dy *= -1;
-                shape.y = shape.r;
-            }
-            if ((shape.y + shape.r) >= graphics.bounds.h) {
-                shape.dy *= -1;
-                shape.y = graphics.bounds.h - shape.r;
-            }
-
-            graphics.set_pen(shape.pen);
-            graphics.circle(Point(shape.x, shape.y), shape.r);
-
-        }
-
-
-        graphics.set_pen(WHITE);
-        graphics.text("Hello World", text_location, 320);
-
-        // update screen
         st7789.update(&graphics);
-
-        i += 10;
-        tufty.led(i);
+        sleep_ms(1000);
     }
-
-    return 0;
 }
